81a6c40607b4b677bf3e2e6bc119bff9c47d5e6c
yanuk88 <yanuk88@gmail.com>
2022-05-06 14:18:55 +0800

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //exercise 1
+        String givenName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName +" " + givenName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //exercise 2
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName1);
+
+        //exercise 3
+        String fullName2 = fullName.replace('ё', 'е');
+        System.out.println(fullName2);
+
+
     }
 }
